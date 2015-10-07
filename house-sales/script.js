a40ad2964dde2f243332a7fc6066720b2f3b3f15
Hsin Ju Lee <clovej03241@yahoo.com.tw>
2015-10-07 23:33:05 +0100

    Use Highstock charts instead of D3

--- house-sales/script.js
+++ house-sales/script.js
@@ -1,60 +1,69 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+$(function () {
+    function setupHighcharts(data) {
+        // skip header row
+        var noHeader = data.filter(function(row, index) {
+            return index > 0;
+        });
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+        var transposedArray = noHeader[0].map(function(col, i) {
+            return noHeader.map(function(row) {
+                return row[i]
+            })
+        });
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+        var postcodes = transposedArray[0]
+        var houseSales = transposedArray[3].map(function (val) { return +val; })
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+        $('#container').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'House sales in different London postcodes'
+            },
+            subtitle: {
+                text: 'Source: Government data'
+            },
+            xAxis: {
+                categories: transposedArray[0],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'House sales'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: 'House sales',
+                data: houseSales
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "");
+            }]
+        });
+    }
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.csv("price-by-postcode-whether-newbuild.csv", type, function(error, data) {
-  if (error) throw error;
-
-  x.domain(data.map(function(d) { return d.post_code_group; }));
-  y.domain([0, d3.max(data, function(d) { return d.sales_no; })]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("House sales");
-
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.post_code_group); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.sales_no); })
-      .attr("height", function(d) { return height - y(d.sales_no); });
+    $.ajax({
+        url: "price-by-postcode-whether-newbuild.csv",
+        async: false,
+        success: function (csvd) {
+            var data = $.csv.toArrays(csvd);
+            setupHighcharts(data);
+        },
+        dataType: "text",
+        complete: function () {}
+    });
 });
-
-function type(d) {
-  d.sales_no = +d.sales_no;
-  return d;
-}
